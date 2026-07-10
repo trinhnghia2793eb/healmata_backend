228c6aff01fa75005750c953b9b5052230e0fce7
trinhnghia2793eb <nghiath@ebizworld.com.vn>
2026-07-10 14:57:57 +0700

    Fix: Email or phone Error Code

--- internal/auth/errors/code.go
+++ internal/auth/errors/code.go
@@ -10,8 +10,8 @@ var (
 	AUTH_REG_003 = NewAppError(http.StatusUnprocessableEntity, "AUTH_REG_003", "INVALID_PASSWORD")
 	AUTH_REG_004 = NewAppError(http.StatusUnprocessableEntity, "AUTH_REG_004", "INVALID_NAME")
 	AUTH_REG_005 = NewAppError(http.StatusInternalServerError, "AUTH_REG_005", "INTERNAL_ERROR")
-	AUTH_REG_006 = NewAppError(http.StatusUnprocessableEntity, "AUTH_REG_006", "INVALID_EMAIL")
-	AUTH_REG_007 = NewAppError(http.StatusUnprocessableEntity, "AUTH_REG_007", "INVALID_PHONE")
+	AUTH_REG_006 = NewAppError(http.StatusUnprocessableEntity, "AUTH_REG_006", "INVALID_IDENTIFIER")
+	AUTH_REG_007 = NewAppError(http.StatusUnprocessableEntity, "AUTH_REG_007", "INVALID_IDENTIFIER")
 	AUTH_REG_008 = NewAppError(http.StatusUnprocessableEntity, "AUTH_REG_008", "CONFIRM_PASSWORD_MISMATCH")
 
 	// Alias for compatibility/readability:
